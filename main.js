612d9e53e3974ed1c115c7a16831ff6ebdd0f179
Ryan Catrine <ryancatrine@gmail.com>
2019-03-31 18:13:25 -0400

    Still working on search function/got audio player to appear.

--- main.js
+++ main.js
@@ -1,36 +1,77 @@
-window.addEventListener('DOMContentLoaded', (event) => { 
-    console.log('DOM loaded and parsed!');
-
-let jackJohnson = fetch("https://itunes-api-proxy.glitch.me/artist/jack-johnson/909253?uo=4")
-    console.log('Fetch functional!')
+document.addEventListener('DOMContentLoaded', function () {
     
-    const input = document.querySelector('#search')
-    console.log(input)
+    const submitButton = document.getElementById('submit')
+    const songList = document.getElementById('songlist')
 
-    // const type = document.querySelector('searchValue')
-    // console.log(type)
-
-    const button = document.querySelector('#button')
-    console.log(button)
-    buttonClick()
+    submitButton.addEventListener('click', event => {
+        
+        console.log("Button clicked.")
+        searchSong()
+    })
+    songList.addEventListener('click', event => {
+        
+        console.log("Song clicked.")
+        if(event.target && event.target.nodeName == "IMG") {
+            
+            console.log("List item ", event.target.id, " was clicked.")
+            const playBar = document.getElementById('audio')
+            songBar.src = event.target.id
+            songBar.play()
+        }
+    })
 })
 
-function buttonClick(button){
-    document.querySelector("button").addEventListener("click",function(event){
-        event.stopPropagation()
-        })
+function searchSong() {
+
+    //Registers input
+    const userInput = document.getElementById('searchinput')
+    const inputError = document.getElementById('inputerror')
+
+    //Resets the song bar
+    const songBar = document.getElementById('audio')
+    songBar.src = ""
+
+    //Gets user input, validates it
+    const userInputValue = userInput.nodeValue
+    if(userInputValue != null && userInputValue != '') {
+        inputError.innerText = ''
+        updateSongs(userInputValue)
     }
+    else {
+        inputError.innerText = "enter a song, album, or artist name"
+    }
+}
 
+function getSongs(userInput) {
+    const promise = fetch(
+        `https://itunes-api-proxy.glitch.me/search?media=music&entity=musicArtist&entity=musicTrack&term=${userInput}&country=US`
+    ).then(function (response) {
+        if (!response.ok) {
+            throw Error(response.statusText)
+        }
+        return response.json()
+    })
+    return promise
+}
 
-
-
-// function searchMusic (searchMusic) {
-//     const promise = fetch(`https://itunes-api-proxy.glitch.me/search?music=${searchMusic}`)
-//     .then(function (response) {
-//         if (!response.ok) {
-//         throw Error(response.musicText)
+//Updates songs in HTML
+// function updateSongs(userInput) {
+//     //Retrieves tracks from iTunes API
+//     getSongs(encodeURIComponent(userInput))
+//     .then(function (songData) {
+//         console.log(songData)
+//         const songListDiv = document.getElementById('songlist')
+//         songListDiv.innerHTML = ""
+//         for(let i=0; i<songData.resultCount; i++){
+//             const result = songData.results[i]
+//             //Creates song element, adds class
+//             const imageHtml = `<img id="${result.previewUrl}" src="${result.artworkUrl100}" />`
+//             const songNameDiv = `<div class="song-name">${result.songCensoredName}</div>`
+//             const songArtistDiv = `<div class="song-artist">${result.artistName}</div>`
+//             let song = document.createElement('div')
+//             song.classList.add('song')
+//             song.innerHTML = `${imageHtml} ${songNameDiv} ${songArtistDiv}`
+//             songListDiv.append(song)
 //         }
-//         return response.json()
-//         })
-//     return promise
+    // })
 // }
